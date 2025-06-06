2cd3d0e2613060bb8c025d070df90fd50bb41007
Hahahaaaaaaa77 <claraae04@gmail.com>
2025-06-06 18:32:13 +0000

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -6,8 +6,8 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            
+            newsletterName: 'RONALD BOT',
             serverMessageId: -1
         }
     }
